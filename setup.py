6996529aa05da7d80d20bb58337e2e2ed0c2a638
John Kelley <johnhkelley1@gmail.com>
2018-11-28 18:47:54 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
+
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in viewports/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('viewports/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='viewports',
 	version=version,
-	description='External Monitor data displays for Frappe',
-	author='Who Agency',
-	author_email='josh@whoagency.com',
+	description='App for displaying data on TVs',
+	author='Evaqua Farms',
+	author_email='support@whoagency.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
-)
+	install_requires=install_requires
